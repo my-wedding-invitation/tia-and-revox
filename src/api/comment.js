c7e296ec10a5ef7b144a6f55740eadd86731cc61
my-wedding-invitation <jehan.intimap@gmail.com>
2023-07-11 13:10:57 +0700

    Use match instead of eq

--- src/api/comment.js
+++ src/api/comment.js
@@ -17,7 +17,7 @@ export default function(api) {
             const { data } = await api
                 .from('comments')
                 .update(comment)
-                .where('uuid', uuid)
+                .match({ uuid })
                 .select()
             return data.length ? data[0] : {}
         },
@@ -25,7 +25,7 @@ export default function(api) {
             const { error } = await supabase
                 .from('comments')
                 .delete()
-                .where('uuid', uuid)
+                .match({ uuid })
             return error
         }
     }
