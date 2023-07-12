f58eafc2ac537251fd1a2b5daaf48769c7d5ea08
my-wedding-invitation <my-wedding-invitation@users.noreply.github.com>
2023-07-12 06:05:34 +0000

    deploy: 7d60ea526b8397ef10188ccee3cb6c6833e78439

---
+++ assets/signin-legacy.677fc4b1.js
@@ -0,0 +1,1 @@
+System.register(["./index-legacy.ec440281.js"],(function(e,n){"use strict";var l;return{setters:[function(e){l=e.n}],execute:function(){e("default",l({},null,null,!1,null,null,null,null).exports)}}}));
--- assets/signin.c185a43c.js
+++
@@ -1,1 +0,0 @@
-import{n}from"./index.44ea1904.js";const l={},s=null,o=null;var t=n(l,s,o,!1,null,null,null,null);const _=t.exports;export{_ as default};
